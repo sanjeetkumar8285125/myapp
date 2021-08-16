3fc8c2172fdb6b7a005a87cfc17c358380075ac2
sanjeetkumar8285125 <55954257+sanjeetkumar8285125@users.noreply.github.com>
2021-08-16 15:31:27 +0530

    Update config.js

--- model/config.js
+++ model/config.js
@@ -1,6 +1,6 @@
 const config={
-    dbURL:'mongodb://localhost:27017/myapp',
-    // dbURL:'mongodb+srv://Sanjeet:12345@cluster0.6rmae.mongodb.net/myapp?retryWrites=true&w=majority',
+   // dbURL:'mongodb://localhost:27017/myapp',
+    dbURL:'mongodb+srv://Sanjeet:12345@cluster0.6rmae.mongodb.net/myapp?retryWrites=true&w=majority',
     poolSize:10
 }
 module.exports=config;
